54aba3aa8a567b77f9ec33c4b868a245420a99b0
Evgeny Timofeyev <nawasington@ya.ru>
2018-12-09 02:28:23 +0300

    добавлены постеры и ссылки на фильмы в кинопоиске

--- cinemas.py
+++ cinemas.py
@@ -51,27 +51,58 @@ def fetch_movie_info_multiple(movie_titles):
     return zip(movie_titles, raw_htmls_list)
 
 
+def get_poster_by_id(movie_id):
+    return "https://www.kinopoisk.ru/images/sm_film/{}.jpg".format(movie_id)
+
+
+def get_url_by_id(movie_id):
+    return "https://www.kinopoisk.ru/film/{}/".format(movie_id)
+
+
+def parse_movie_from_self_page(soup, title):
+    none_int = 0
+    rating_tag = soup.select_one('span.rating_ball')
+    count_tag = soup.select_one('span.ratingCount')
+
+    rating = float(rating_tag.string) if rating_tag else none_int
+    votes_cnt = int(count_tag.string) if count_tag else none_int
+
+    return {
+        "title": title,
+        "rating": rating,
+        "votes_cnt": votes_cnt,
+        "poster": '',
+        "url": ''
+    }
+
+
 def parse_movie_info_multiple(titles_htmls_tuples_list):
     movies = []
     for title, raw_html in titles_htmls_tuples_list:
         soup = BeautifulSoup(raw_html, features="html.parser")
-        rating_tag = soup.select_one('div.element.most_wanted div.rating')
-        none_int = 0
+        element_tag = soup.select_one('div.element.most_wanted')
+        rating_tag = element_tag.select_one('div.rating')
         if rating_tag is None:
-            rating_tag = soup.select_one('span.rating_ball')
-            count_tag = soup.select_one('span.ratingCount')
-            rating = float(rating_tag.string) if rating_tag else none_int
-            votes_cnt = int(count_tag.string) if count_tag else none_int
-            movies.append(
-                {"title": title, "rating": rating, "votes_cnt": votes_cnt}
-            )
+            movie = parse_movie_from_self_page(soup, title)
+            movies.append(movie)
             continue
 
+        title_tag = element_tag.select_one('a.js-serp-metrika')
+        movie_id = title_tag['data-id']
         rating, votes_cnt = rating_tag['title'].split(' ')
         rating = float(rating)
         votes_cnt = int(re.sub(r'[()\u00a0]', '', votes_cnt))
+
+        poster_url = get_poster_by_id(movie_id)
+        movie_url = get_url_by_id(movie_id)
         movies.append(
-            {"title": title, "rating": rating, "votes_cnt": votes_cnt}
+            {
+                "title": title,
+                "rating": rating,
+                "votes_cnt": votes_cnt,
+                "poster": poster_url,
+                "url": movie_url
+            }
         )
     return movies
 
